57d2f0e95ad77f04a0f3df0cbd79a8d2c8675607
thunchaaaaa <thunchanok_du@cmu.ac.th>
2021-08-05 13:54:49 +0700

    ส่งงาน
    ส่งงาน

--- compile/interface.js
+++ compile/interface.js
@@ -1,13 +1,28 @@
 "use strict";
 function createSquare(config) {
-    var newSquare = { coler: "white", area: 10000 };
-    if (config.color) {
-        newSquare.color = config.color;
+    var newemp = { fname: "default first name", lname: "default lastname", code: 1111, age: 24, salary: 0, bonus: true };
+    if (config.fname) {
+        newemp.fname = config.fname;
     }
-    if (config.width) {
-        newSquare.area = config.width * config.width;
+    if (config.lname) {
+        newemp.lname = config.lname;
     }
-    return newSquare;
+    if (config.code) {
+        newemp.code = config.code;
+    }
+    if (config.age) {
+        newemp.age = config.age;
+    }
+    if (config.salary) {
+        newemp.salary = config.salary;
+    }
+    if (config.bonus) {
+        newemp.bonus = config.bonus;
+    }
+    if (config.bonus == true) {
+        newemp.salary = newemp.salary * 1.25;
+    }
+    return newemp;
 }
-var mySquare = createSquare({ color: "black", width: 100 });
-console.log("mySquare", mySquare);
+var myemp = createSquare({ fname: "sirawat", lname: "topanyaruang", code: 632110355, age: 24, salary: 10000, bonus: true });
+console.log("myemp", myemp);
--- src/interface.ts
+++ src/interface.ts
@@ -1,21 +1,36 @@
 interface SquareConfig {
-    color?: String;
-    width?: number;
+    fname: string;
+    lname: string;
+    code: number;
+    age: number;
+    salary:number;
+    bonus: boolean;
 }
 
-function createSquare(config:SquareConfig): {color: string; area: number} {
-    let newSquare = {coler: "white", area: 10000}
-
-    if (config.color){
-        newSquare.color = config.color
+function createSquare(config:SquareConfig):{fname: string,lname: string,code: number,age:number,salary:number,bonus:boolean}{
+    let newemp ={fname:"default first name",lname:"default lastname",code:1111,age:24,salary:0,bonus:true}
+    if(config.fname){
+        newemp.fname=config.fname
     }
-
-    if(config.width) {
-        newSquare.area = config.width * config.width
-        
+    if(config.lname){
+        newemp.lname=config.lname
     }
-    return newSquare
+    if(config.code){
+        newemp.code=config.code
+    }
+    if(config.age){
+        newemp.age=config.age
+    }
+    if(config.salary){
+        newemp.salary=config.salary
+    }
+    if(config.bonus){
+        newemp.bonus=config.bonus
+    }
+    if(config.bonus==true){
+        newemp.salary=newemp.salary*1.25
+    }
+    return newemp
 }
-let mySquare = createSquare({color:"black",width:100})
-
-console.log(`mySquare`,mySquare)
+let myemp = createSquare({fname:"sirawat",lname:"topanyaruang",code:632110355,age:24,salary:10000,bonus:true})
+console.log(`myemp`,myemp)
